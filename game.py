dbd2b092422a6b49803e71c91f55ba3a01d7cf70
sennaheadley <89592686+sennaheadley@users.noreply.github.com>
2021-09-24 15:40:17 -0400

    comment out breakpoint

--- game.py
+++ game.py
@@ -1,11 +1,10 @@
 # this is the "game.py" file...
+import random
 
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
-
-user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 print(user_choice)
 
-import random
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
@@ -14,40 +13,40 @@ print(computer_choice)
 #breakpoint()
 
 if user_choice == "rock" and computer_choice == "scissors":
-    print("You chose",user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You won this round!")
 elif user_choice == "rock" and computer_choice == "paper":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You lost this round!")
 elif user_choice == "rock" and computer_choice == "rock":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You tied this round!")
 if user_choice == "paper" and computer_choice == "rock":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You won this round!")
 elif user_choice == "paper" and computer_choice == "scissors":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You lost this round!")
 elif user_choice == "paper" and computer_choice == "paper":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You tied this round!")
 if user_choice == "scissors" and computer_choice == "paper":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You won this round!")
 elif user_choice == "scissors" and computer_choice == "rock":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You lost this round!")
 elif user_choice == "scissors" and computer_choice == "scissors":
-    print("You chose", user_choice,")
-    print("The computer chose", computer_choice,")
+    print("You chose",user_choice)
+    print("The computer chose", computer_choice)
     print("You tied this round!")
 
 print("Thanks for playing.")
