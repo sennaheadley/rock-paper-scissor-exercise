94f0a694d0e3a63e10ef09349ee27691f69c0775
sennaheadley <89592686+sennaheadley@users.noreply.github.com>
2021-09-26 12:00:45 -0400

    Final wording edits

--- game.py
+++ game.py
@@ -10,8 +10,7 @@ playername = os.getenv("PLAYER_NAME")
 print("Welcome", playername, "to my Rock-Paper-Scissors game...")
 user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 if user_choice in ['rock','paper','scissors']:
-    print("user chose:") 
-    print(user_choice)
+    print("You chose:", user_choice) 
 else: 
     print("Your choice is not valid.  Reminder: all letters should be lowercase!")
     print("Try again.")
@@ -21,45 +20,26 @@ else:
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
-print("computer chose:")
-print(computer_choice)
+print("Computer chose:", computer_choice)
 
 
 if user_choice == "rock" and computer_choice == "scissors":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You won this round!")
 elif user_choice == "rock" and computer_choice == "paper":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You lost this round!")
 elif user_choice == "rock" and computer_choice == "rock":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You tied this round!")
 if user_choice == "paper" and computer_choice == "rock":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You won this round!")
 elif user_choice == "paper" and computer_choice == "scissors":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You lost this round!")
 elif user_choice == "paper" and computer_choice == "paper":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You tied this round!")
 if user_choice == "scissors" and computer_choice == "paper":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You won this round!")
 elif user_choice == "scissors" and computer_choice == "rock":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You lost this round!")
 elif user_choice == "scissors" and computer_choice == "scissors":
-    print("You chose",user_choice)
-    print("The computer chose", computer_choice)
     print("You tied this round!")
 
 print("Thanks for playing.")
